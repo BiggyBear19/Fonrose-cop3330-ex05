5a30b0705c8ae5d352dcf7e398b9c26b044b450a
BiggyBear19 <bernardthebear19@gmail.com>
2021-09-13 18:58:42 -0400

    updated

--- src/main/java/Exercise5/Exercise5.java
+++ src/main/java/Exercise5/Exercise5.java
@@ -1,2 +1,29 @@
-package Exercise5;public class Exercise5 {
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Bernard Fonrose
+ */
+
+package Exercise5;
+
+import java.util.Scanner;
+
+public class Exercise5 {
+    public static void main(String[] args){
+
+        Scanner math = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+
+        String number1 = math.nextLine();
+
+        var num1 = Integer.parseInt(number1, 10);
+
+        System.out.print("Enter another number: ");
+
+        String number2 = math.nextLine();
+
+        var num2 = Integer.parseInt(number2, 10);
+
+        System.out.printf("%d + %d = %d\n%d - %d = %d\n%d * %d = %d\n%d / %d = %d\n",num1, num2,  (num1 + num2), num1, num2, (num1 - num2), num1, num2, (num1 * num2), num1, num2, (num1 / num2));
+    }
 }
